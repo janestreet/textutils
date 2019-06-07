1a09da428fba22eb67a2292e96485c2af3581cdf
Xavier Clerc <xclerc@janestreet.com>
2019-06-07 09:36:45 +0100

    v0.13-preview.120.39+175

--- ascii_table/src/ascii_table.mli
+++ ascii_table/src/ascii_table.mli
@@ -17,11 +17,21 @@ end
 
 module Display : sig
   type t
-  val short_box     : t (* Default--cells can be multi-line.   *)
-  val tall_box      : t (* Puts --- between entries.           *)
-  val line          : t (* Trails off with ... if necessary.   *)
-  val blank         : t (* No lines                            *)
-  val column_titles : t (* Draw lines only under column titles *)
+
+  (** Default--cells can be multi-line. *)
+  val short_box : t
+
+  (** Puts --- between entries. *)
+  val tall_box : t
+
+  (** Trails off with ... if necessary. *)
+  val line : t
+
+  (** No lines. *)
+  val blank : t
+
+  (** Draw lines only under column titles. *)
+  val column_titles : t
 end
 
 module Column : sig
@@ -29,36 +39,36 @@ module Column : sig
 
   (** creates a column given the header and the to-string function *)
   val create
-    :  ?align : Align.t (* Default: left *)
-    -> ?min_width : int
-    -> ?max_width : int
-    -> ?show : [ `Yes | `No | `If_not_empty ] (* Default: `Yes *)
+    :  ?align:Align.t (* Default: left *)
+    -> ?min_width:int
+    -> ?max_width:int
+    -> ?show:[`Yes | `No | `If_not_empty] (* Default: `Yes *)
     -> string
     -> ('a -> string)
     -> 'a t
 
   (** like create, except that the to_string function must provide a list of
       attributes. *)
-  val create_attr:
-    ?align : Align.t (* Default: left *)
-    -> ?min_width : int
-    -> ?max_width : int
-    -> ?show : [ `Yes | `No | `If_not_empty ] (* Default: `Yes *)
+  val create_attr
+    :  ?align:Align.t (* Default: left *)
+    -> ?min_width:int
+    -> ?max_width:int
+    -> ?show:[`Yes | `No | `If_not_empty] (* Default: `Yes *)
     -> string
     -> ('a -> Attr.t list * string)
     -> 'a t
 
-  val header  : 'a t ->                                string
+  val header : 'a t -> string
   val to_data : 'a t -> 'a -> Console.Ansi.attr list * string list
 end
 
 type ('row, 'rest) renderer =
-  ?display : Display.t (* Default: short_box *)
-  -> ?spacing : int (* Default: 1 *)
-  -> ?limit_width_to : int (* defaults to 90 characters *)
-  -> ?header_attr : Attr.t list
-  -> ?bars : [ `Ascii | `Unicode ] (* defaults to [`Unicode] *)
-  -> ?display_empty_rows : bool (* Default: false *)
+  ?display:Display.t (* Default: short_box *)
+  -> ?spacing:int (* Default: 1 *)
+  -> ?limit_width_to:int (* defaults to 90 characters *)
+  -> ?header_attr:Attr.t list
+  -> ?bars:[`Ascii | `Unicode] (* defaults to [`Unicode] *)
+  -> ?display_empty_rows:bool (* Default: false *)
   -> 'row Column.t list
   -> 'row list
   -> 'rest
@@ -72,15 +82,20 @@ val output : (_, oc:Out_channel.t -> unit) renderer
 val to_string_noattr : (_, string) renderer
 val to_string : (_, string) renderer
 
-val simple_list_table : ?index:bool -> ?limit_width_to:int ->
-  ?oc:Out_channel.t -> ?display:Display.t ->
-  string list -> string list list -> unit
+val simple_list_table
+  :  ?index:bool
+  -> ?limit_width_to:int
+  -> ?oc:Out_channel.t
+  -> ?display:Display.t
+  -> string list
+  -> string list list
+  -> unit
 
 module Table_char : sig
-  type t = {
-    ascii : char;
-    utf8 : string;
-  }
+  type t =
+    { ascii : char
+    ; utf8 : string
+    }
 
   val connect : ?top:unit -> ?bottom:unit -> ?left:unit -> ?right:unit -> unit -> t
 end
