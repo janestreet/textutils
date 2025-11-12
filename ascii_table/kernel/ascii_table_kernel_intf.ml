ee01155b4caff7b0f3360e7539ae30e10bda4516
public-release <as-public-release@janestreet.com>
2025-11-12 20:37:29 +0000

    v0.18~preview.130.74+175

--- ascii_table/kernel/ascii_table_kernel_intf.ml
+++ ascii_table/kernel/ascii_table_kernel_intf.ml
@@ -71,6 +71,7 @@ module type Ascii_table_kernel = sig @@ portable
     :  ?display:Display.t (** Default: short_box *)
     -> ?spacing:int (** Default: 1 *)
     -> ?limit_width_to:int (** defaults to 90 characters *)
+    -> ?header_attr:Attr.t list (** defaults to empty list *)
     -> ?display_empty_rows:bool (** Default: false *)
     -> ?prefer_split_on_spaces:bool (** Default: false *)
     -> 'row Column.t list
@@ -84,6 +85,7 @@ module type Ascii_table_kernel = sig @@ portable
     -> ?spacing:int (** Default: 1 *)
     -> ?limit_width_to:int (** defaults to 160 characters *)
     -> ?max_col_width:int (** defaults to 90 characters *)
+    -> ?header_attr:Attr.t list (** defaults to empty list *)
     -> ?bars:[ `Ascii | `Unicode ] (** default: `Unicode *)
     -> ?display_empty_rows:bool (** Default: false *)
     -> ?prefer_split_on_spaces:bool (** Default: false *)
